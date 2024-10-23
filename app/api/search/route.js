589733685294e7a43809f6b0649b326b1318441b
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2024-10-23 13:13:42 +0530

    search api

---
+++ app/api/search/route.js
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const query = request.nextUrl.searchParams.get("query");
+  console.log("Query:", query); // Log the query parameter
+
+  // Replace the uri string with your connection string.
+  const uri =
+    "mongodb+srv://prarthanab1703:anahtrarP17@manage.ncqyr.mongodb.net/";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect(); // Ensure to connect to the database
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
+
+    const products = await inventory
+      .aggregate([
+        {
+          $match: {
+            $or: [{ slug: { $regex: query, $options: "i" } }],
+          },
+        },
+      ])
+      .toArray();
+
+    return NextResponse.json({ success: true, products });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json({ success: false, error: error.message });
+  } finally {
+    await client.close(); // Ensure the client closes
+  }
+}
